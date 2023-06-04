c80778ef08bd0205fd2794a705840bfadb1dbcf4
camilogs <camilo.gonzalez.col@gmail.com>
2023-06-03 20:00:03 -0500

    Extract creations of arrange components to private methods

--- Tests/CEManagerTests.cs
+++ Tests/CEManagerTests.cs
@@ -5,6 +5,7 @@ using CEM.DataAccess;
 using CEM.Context;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using CEM.Repositories;
 
 namespace CEM.Tests;
 
@@ -27,27 +28,38 @@ public class CEManagerTests
         {
             dbContext.Database.EnsureCreated();     
 
-            // Arrange 
-            ITransactionData transactionData1 = new TransactionData();
-            transactionData1.SetData("NewCategory" ,"transactionDescription", "1000");
-            transactionData1.SetRequestType(RequestType.Income);
-            var transactionDataAccess = new EFTransactionDataAccess(dbContext);
-            var categoryDataAccess = new EFCategoryDataAccess(dbContext);
-            CEManager manager = new CEManager(transactionDataAccess, categoryDataAccess);
 
-            // Act
-            manager.MakeTransaction(transactionData1);
+            ITransactionData transactionData = CreateTransactionData("NewCategory" ,"transactionDescription", "1000", RequestType.Income);
+            CEManager manager =  CreateManager(dbContext);
 
+            manager.MakeTransaction(transactionData);
 
-            // Assert
             var createdCategory = dbContext.Categories.FirstOrDefault(c => c.Name == "NewCategory");
             var transactionsInCategory = createdCategory?.TransactionsInCategory.FirstOrDefault(t =>
                 t.Description == "transactionDescription" &&
                 t.Amount == 1000);
-
             Assert.NotNull(transactionsInCategory);
+
 
             dbContext.Database.EnsureDeleted();
         }
+    }
+
+    private ITransactionData CreateTransactionData(string category, string description, string amount, RequestType requestType)
+    { 
+        ITransactionData transactionData = new TransactionData();
+        transactionData.SetData(category ,description, amount);
+        transactionData.SetRequestType(requestType);
+
+        return transactionData;
+    }
+
+    private CEManager CreateManager(InMemoryDbContext dbContext)
+    {
+        ITransactionRepository transactionRepository = new EFTransactionDataAccess(dbContext);
+        ICategoryRepository categoryRepository= new EFCategoryDataAccess(dbContext);
+        var manager = new CEManager(transactionRepository, categoryRepository);
+
+        return manager;
     }
 }
