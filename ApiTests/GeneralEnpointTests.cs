b9a63e19c97636de4f212226fe23af91a9210524
camilogs <camilo.gonzalez.col@gmail.com>
2023-10-31 12:34:19 -0500

    Create structure of new architecture

---
+++ ApiTests/GeneralEnpointTests.cs
@@ -0,0 +1,32 @@
+using AutoCallsApi.Data.EntityFramework;
+using ApiTests.Helpers;
+
+namespace IntegrationTests;
+
+public partial class EnpointsTests 
+    : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private readonly CustomWebApplicationFactory<Program> _factory;
+    private readonly EfApplicationDbContext _context;
+
+    public EnpointsTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        _context = DbContextUtilities.GetDbContext(factory);
+        DbUtilities.ReinitializeDbForTests(_context);
+    }
+
+    [Theory]
+    [InlineData("/api/number")]
+    [InlineData("/api/masiveCall")]
+    public async Task ClientGetAll_ReturnSuccessTest(string url)
+    {
+        var client = _factory.CreateClient();
+
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+        Assert.Equal("application/json; charset=utf-8", 
+            response.Content.Headers.ContentType?.ToString());
+    }
+}
---
+++ ApiTests/TransactionTest.cs
@@ -0,0 +1,10 @@
+namespace ApiTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
